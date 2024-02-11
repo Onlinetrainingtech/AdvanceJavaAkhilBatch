d4ee5cdd689131511b30d4bb77fe674c4fd45108
azartechnet <azartechnet@gmail.com>
2024-02-11 15:14:15 +0530

    updated

---
+++ Nodejs/src/main/webapp/Example1.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+ 
+ function number(salary)
+{
+    if(salary>2000)
+    {
+        console.log("The salary is greater than 2000");
+    }
+    else
+    {
+        console.log("The salary is not greater than 2000")
+    }
+}
+number(4000);
